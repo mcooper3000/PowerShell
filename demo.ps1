849123ea8bc7d6cb1bd09bcba4cede56dfb0eeb6
mcooper3000 <mcooper3000@gmail.com>
2017-04-04 22:51:18 +0100

    Add files via upload

---
+++ demo.ps1
@@ -0,0 +1,14 @@
+﻿$t = Get-Service | Where-Object {$_.StartType -match "auto"}
+
+#Some comment here 
+if ($env:COMPUTERNAME -eq "OFFICE-LAPTOP") {Write-Host "Match computer name $env:COMPUTERNAME"}
+else {
+    if (Test-Path $env:SystemDrive) {
+        Write-Output "$env:SystemDrive exists"
+    }
+    Write-Host "Fail"
+}
+
+Get-Process
+
+#something 
