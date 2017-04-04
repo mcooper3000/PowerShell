a217d2a8dcfef7a44f66e35a058de2507b042001
Marc Cooper <marc@cooper3000.co.uk>
2017-04-05 00:22:12 +0100

    change to Get-ACL C:

---
+++ temp/demo.ps1
@@ -0,0 +1,18 @@
+
+$t = Get-Service | Where-Object {$_.StartType -match "auto"}
+
+#Some comment here 
+if ($env:COMPUTERNAME -eq "OFFICE-LAPTOP") {Write-Host "Match computer name $env:COMPUTERNAME"}
+else {
+    if (Test-Path $env:SystemDrive) {
+        Write-Output "$env:SystemDrive exists"
+    }
+    Write-Host "Fail"
+}
+
+Get-Process
+Write-Host "This is a statement"
+#something 
+
+
+Get-Acl C:\
